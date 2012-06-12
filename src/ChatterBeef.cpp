f470298f01d93db841ed26a95814e0be28542205
beef <met898@tamu.edu>
2012-06-12 10:12:04 -0500

    Initial commit

---
+++ src/ChatterBeef.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : ChatterBeef.cpp
+// Author      : bearpaw7
+// Version     :
+// Copyright   : MIT License
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <pthread.h>
+
+using namespace std;
+
+int main() {
+	cout << "hello world" << endl; // prints hello world
+	return 0;
+}
